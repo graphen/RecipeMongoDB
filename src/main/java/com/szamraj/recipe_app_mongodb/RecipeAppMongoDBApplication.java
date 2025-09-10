0fd364180fd9b6b5b01c4b2fa9046a5b435e36d3
Przemek Szamraj <przemyslaw.szamraj@pl.ibm.com>
2025-09-10 18:41:29 +0200

    Add main application classes

---
+++ src/main/java/com/szamraj/recipe_app_mongodb/RecipeAppMongoDBApplication.java
@@ -0,0 +1,13 @@
+package com.szamraj.recipe_app_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppMongoDBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAppMongoDBApplication.class, args);
+	}
+
+}
